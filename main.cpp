fa12272e8e5783db0f4373b2b495238655cc6887
Alessasndro <valentin.enache@edu.unifi.it>
2025-10-17 10:57:50 +0200

    creazione e configurazione iniziale progetto su GIT-repository, sfml mediante fetchcontent (collegamento via link) , installazione automatica delle .dll (libreria sfml in formato Dinamic) main con apertura della finestra (using namespace sf per non doverlo scrivere sempre )

--- main.cpp
+++ main.cpp
@@ -1,2 +1,19 @@
-#include <iostream>
-//start
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
+int main() {
+    RenderWindow window(VideoMode(800, 600), "SFML con CMake");
+
+    while (window.isOpen()) {
+        Event event{};
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
+
+    return 0;
+}
